8c909870825fe8c888d30619a6c04d54ae6d4cf2
Leandro Daniel <contato@leandrodaniel.com>
2019-08-02 09:35:33 -0300

    Update createTestAndTrainSamples.R

--- R/createTestAndTrainSamples.R
+++ R/createTestAndTrainSamples.R
@@ -10,16 +10,16 @@
 createTestAndTrainSamples <- function(dataset, y_var) {
   set.seed(12345)
   
-  dataset[y_var] <- as.integer(unlist(dataset[y_var]))
+  dataset[[y_var]] <- as.integer(dataset[[y_var]])
   
-  index <- caret::createDataPartition(dataset[y_var], p = 0.7, list = FALSE)
+  index <- caret::createDataPartition(dataset[[y_var]], p = 0.7, list = FALSE)
   data.train <- dataset[index, ]
   data.test  <- dataset[-index,]
   
   # checking event proportion in sample and test datasets against full dataset.
-  event_proportion <- bind_rows(prop.table(table(dataset[y_var])),
-                                prop.table(table(dataset[y_var])),
-                                prop.table(table(dataset[y_var])))
+  event_proportion <- bind_rows(prop.table(table(dataset[[y_var]])),
+                                prop.table(table(data.train[[y_var]])),
+                                prop.table(table(data.test[[y_var]])))
   
   event_proportion$scope = ''
   event_proportion$scope[1] = 'full dataset'
@@ -28,5 +28,10 @@ createTestAndTrainSamples <- function(dataset, y_var) {
   
   event_proportion <- select(event_proportion, scope, everything())
   
-  SplitDataset <- list()
+  split_dataset <- list()
+  split_dataset$data.train <- data.train
+  split_dataset$data.test  <- data.test
+  split_dataset$event.proportion <- event_proportion
+  
+  return(split_dataset)
 }
