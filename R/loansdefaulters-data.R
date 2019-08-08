b36ed25613273ae104f770a8e67add789924276a
ldaniel <contato@leandrodaniel.com>
2019-08-07 22:52:17 -0300

    Adding a dataset in the package

---
+++ R/loansdefaulters-data.R
@@ -0,0 +1,21 @@
+#' Loans defaulters dataset
+#'
+#' Dataset developed from a set of models using a real anonymized Czech bank 
+#' transactions, account info, and loan records released for PKDD'99 Discovery Challenge.
+#'
+#' @docType data
+#'
+#' @usage data(loansdefaulters)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Predictive-Analytics FGV's work assignment
+#' (\href{https://ldaniel.github.io/Predictive-Analytics/}{Predictive-Analytics})
+#'
+#' @source \href{https://github.com/ldaniel/Predictive-Analytics}{Predictive-Analytics source}
+#'
+#' @examples
+#' mydataset <- data(loansdefaulters)
+"loansdefaulters"
