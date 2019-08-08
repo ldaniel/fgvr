24143de8fcf0c811519e93d66248c052e7797100
ldaniel <contato@leandrodaniel.com>
2019-08-08 07:45:46 -0300

    Initial tests

--- tests/testthat/test-createTestAndTrainSamples.R
+++ tests/testthat/test-createTestAndTrainSamples.R
@@ -1,5 +1,11 @@
 context("create test and train samples")
+library(fgvr)
 
 test_that("function returns 3 items in the list", {
-  expect_equal(1, 1)
+  mydataset <- fgvr::createTestAndTrainSamples(fgvr::loansdefaulters, "y_loan_defaulter", 12345, 0.7)
+  
+  expect_equal(length(mydataset), 3)
+  expect_equal(typeof(mydataset$data.train), "list")
+  expect_equal(typeof(mydataset$data.test), "list")
+  expect_equal(typeof(mydataset$event.proportion), "list")
 })
