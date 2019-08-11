c86265c139b6424404998219349370d96d24b3b0
ldaniel <contato@leandrodaniel.com>
2019-08-11 19:11:50 -0300

    Update

--- R/createProjectFromTemplate.R
+++ R/createProjectFromTemplate.R
@@ -4,11 +4,12 @@
 #' @author Leandro Daniel
 #' @import downloader
 #' @param name The name of you R project (it will be created a directory using this name).
+#' @param directorypath The directory path in which the project will be created.
 #' @return An object list with handy information about the project, like path and initial setup.
 #' @keywords project, data-science, setup 
 #' @export
 #' @examples
-#' createProject("Predictive-Analytics")
+#' createProjectFromTemplate("Predictive-Analytics", "c:/temp")
 createProjectFromTemplate <- function(name, 
                                       directorypath = NULL) {
   
@@ -42,6 +43,9 @@ createProjectFromTemplate <- function(name,
     # unzip file to the target directory path
     unzip(zipfile = zipFilePath, exdir = targetDirectoryPath)
     
+    # remove project template zip file
+    unlink(zipFilePath)
+    
     projectDataDirectory          <- paste(targetDirectoryPath,  "data", sep = "/")
     projectDataRawDirectory       <- paste(projectDataDirectory, "raw", sep = "/")
     projectDataProcessedDirectory <- paste(projectDataDirectory, "processed", sep = "/")
@@ -49,20 +53,22 @@ createProjectFromTemplate <- function(name,
     projectMarkdownDirectory      <- paste(targetDirectoryPath,  "markdown", sep = "/")
     projectModelsDirectory        <- paste(targetDirectoryPath,  "models", sep = "/")
     projectScriptsDirectory       <- paste(targetDirectoryPath,  "scripts", sep = "/")
+    
+    # creating the list with train, test and proportion results
+    project <- list()
+    project$path                <- currentDirectoryPath
+    project$data.path           <- projectDataDirectory
+    project$data.raw.path       <- projectDataRawDirectory
+    project$data.processed.path <- projectDataProcessedDirectory
+    project$images.path         <- projectImagesDirectory
+    project$markdown.path       <- projectMarkdownDirectory
+    project$models.path         <- projectModelsDirectory
+    project$scripts.path        <- projectScriptsDirectory
   } else {
-    print(paste0("The directory already exists: ", targetDirectoryPath))
+    print(paste0("The directory already exists and the project will not be created: ", 
+                 targetDirectoryPath))
+    project <- NULL
   }
-  
-  # creating the list with train, test and proportion results
-  project <- list()
-  project$path                <- currentDirectoryPath
-  project$data.path           <- projectDataDirectory
-  project$data.raw.path       <- projectDataRawDirectory
-  project$data.processed.path <- projectDataProcessedDirectory
-  project$images.path         <- projectImagesDirectory
-  project$markdown.path       <- projectMarkdownDirectory
-  project$models.path         <- projectModelsDirectory
-  project$scripts.path        <- projectScriptsDirectory
   
   return(project)
 }
--- tests/testthat/test-createProjectFromTemplate.R
+++ tests/testthat/test-createProjectFromTemplate.R
@@ -3,8 +3,6 @@ library(fgvr)
 
 test_that("function returns a list object containing all the directories path", {
   
-  createProject("test", "d:/Temp")
-  
   ##fgvr::createProject("test", "d:/Temp")
   
   # expect_equal(length(mydataset), 3)
