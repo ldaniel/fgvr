e26211ff7a612c912383fc8435de15359795e019
ldaniel <contato@leandrodaniel.com>
2019-08-12 17:08:10 -0300

    Update test

--- tests/testthat/test-createProjectFromTemplate.R
+++ tests/testthat/test-createProjectFromTemplate.R
@@ -8,19 +8,15 @@ test_that("function returns a list object containing all the directories path", 
   myproject <- fgvr::createProjectFromTemplate(projectname, projectpath)
   
   expect_equal(length(myproject), 8)
-  # expect_equal(myproject$path, projectpath)
-  # expect_equal(myproject$data.path, paste(projectpath,"data", sep = "/"))
-  # expect_equal(myproject$data.raw.path, paste(projectpath, "data/raw", sep = "/"))
-  # expect_equal(myproject$data.processed.path, paste(projectpath, "data/processed", sep = "/"))
-  # expect_equal(myproject$images.path, paste(projectpath, "images", sep = "/"))
-  # expect_equal(myproject$markdown.path, paste(projectpath, "markdown", sep = "/"))
-  # expect_equal(myproject$models.path, paste(projectpath, "models", sep = "/"))
-  # expect_equal(myproject$scripts.path, paste(projectpath, "scripts", sep = "/"))
+  expect_equal(myproject$path, paste(projectpath, projectname, sep = "/"))
+  expect_equal(myproject$data.path, paste(projectpath, projectname, "data", sep = "/"))
+  expect_equal(myproject$data.raw.path, paste(projectpath, projectname, "data/raw", sep = "/"))
+  expect_equal(myproject$data.processed.path, paste(projectpath, projectname, "data/processed", sep = "/"))
+  expect_equal(myproject$images.path, paste(projectpath, projectname, "images", sep = "/"))
+  expect_equal(myproject$markdown.path, paste(projectpath, projectname, "markdown", sep = "/"))
+  expect_equal(myproject$models.path, paste(projectpath, projectname, "models", sep = "/"))
+  expect_equal(myproject$scripts.path, paste(projectpath, projectname, "scripts", sep = "/"))
   
-  unlink(paste(projectpath, projectname, sep = "/"))
-  
-  if(dir.exists(paste(projectpath, projectname, sep = "/")))
-  {
-    do.call(file.remove, list(list.files(paste(projectpath, projectname, sep = "/"), full.names = TRUE)))
-  }
+  # delete project created
+  unlink(paste(projectpath, projectname, sep = "/"), recursive = TRUE)
 })
