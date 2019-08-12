da0e539432b9bc39bfcba33ea4bc90ae3cdce82c
ldaniel <contato@leandrodaniel.com>
2019-08-12 16:31:52 -0300

    Update

--- tests/testthat/test-createProjectFromTemplate.R
+++ tests/testthat/test-createProjectFromTemplate.R
@@ -3,10 +3,22 @@ library(fgvr)
 
 test_that("function returns a list object containing all the directories path", {
   
-  ##fgvr::createProject("test", "d:/Temp")
+  projectname <- "myteste"
+  projectpath <- "d:/Temp"
+  myproject <- fgvr::createProjectFromTemplate(projectname, projectpath)
   
-  # expect_equal(length(mydataset), 3)
-  # expect_equal(typeof(mydataset$data.train), "list")
-  # expect_equal(typeof(mydataset$data.test), "list")
-  # expect_equal(typeof(mydataset$event.proportion), "list")
+  expect_equal(length(myproject), 8)
+  # expect_equal(myproject$path, projectpath)
+  # expect_equal(myproject$data.path, paste(projectpath,"data", sep = "/"))
+  # expect_equal(myproject$data.raw.path, paste(projectpath, "data/raw", sep = "/"))
+  # expect_equal(myproject$data.processed.path, paste(projectpath, "data/processed", sep = "/"))
+  # expect_equal(myproject$images.path, paste(projectpath, "images", sep = "/"))
+  # expect_equal(myproject$markdown.path, paste(projectpath, "markdown", sep = "/"))
+  # expect_equal(myproject$models.path, paste(projectpath, "models", sep = "/"))
+  # expect_equal(myproject$scripts.path, paste(projectpath, "scripts", sep = "/"))
+  
+  if(dir.exists(projectname))
+  {
+    do.call(file.remove, list(list.files(projectname, full.names = TRUE)))
+  }
 })
